88836d4ff91c68c0ffcb27dd9383713b636b2772
sooyeon <ally5336@duksung.ac.kr>
2025-10-12 10:14:17 +0900

    [level 2] Title: 요격 시스템, Time: 351.44 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -0,0 +1,10 @@
+def solution(targets):
+    targets.sort(key=lambda x: x[1])
+    count = 0
+    last = -1
+
+    for s, e in targets:
+        if s >= last:
+            count += 1
+            last = e
+    return count
